c18debf25d566e7985b78cc20e299391776e7202
Fabian Allel <the_pianist2@hotmail.com>
2022-03-02 20:34:25 +0100

    Update server.js
    despliegue aws

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const app = express()
-const port = 3000
+const port = 80
 import fs from 'fs';
 import path from 'path';
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import App from '../src/App'
+import http from 'http';
 
 app.use('^/$', function (req, res, next) {
     fs.readFile(path.resolve('./build/index.html'), 'utf-8', function (err, data) {
@@ -20,6 +21,12 @@ app.use('^/$', function (req, res, next) {
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')))
 
-app.listen(port, () => {
+http.createServer(app)
+.listen(port, "ec2-34-226-148-180.compute-1.amazonaws.com", ()=>{
+    console.log(`Aplicación corriendo en http://:${port}`);
+});
+
+
+/*app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+})*/
